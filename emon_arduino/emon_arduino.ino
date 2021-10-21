67bd1e5ab631a0fe61021b3216b8598becd28860
kareemtawab <kareem.tawab@gmail.com>
2021-10-21 12:58:24 +0200

    update

--- emon_arduino/emon_arduino.ino
+++ emon_arduino/emon_arduino.ino
@@ -1,19 +1,15 @@
 #include "EmonLib.h"
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
 #include "Timer.h"
+#include <SoftwareSerial.h>
 
+#define LED_PIN 13
 #define PHASE_SEQ_PIN 0
 #define READVCC_CALIBRATION_CONST 1180000L
 #define CALC_ENERGY_INTERVAL 5 // in secs
 
 Timer timer;
-
-RF24 radio(9, 10); // CE, CSN
-const byte address[6] = "00001";
 
 // Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS 2
@@ -27,6 +23,8 @@ DallasTemperature DS18B20(&oneWire);
 EnergyMonitor emon1;             // Create an instance
 EnergyMonitor emon2;             // Create an instance
 EnergyMonitor emon3;             // Create an instance
+
+SoftwareSerial ESPserial(11, 12); // RX, TX
 
 int P1_realPower;
 float P1_powerFactor;
@@ -43,7 +41,7 @@ float P3_powerFactor;
 int P3_supplyVoltage;
 float P3_Irms;
 
-int temp;
+float temp;
 
 float P1_energy;
 float P2_energy;
@@ -51,6 +49,11 @@ float P3_energy;
 int error;
 
 void emonget() {
+  DS18B20.requestTemperatures(); // Send the command to get temperatures
+  temp = DS18B20.getTempCByIndex(0);
+  DS18B20.setWaitForConversion(false);
+  DS18B20.requestTemperatures();
+
   emon1.calcVI(20, 2000);        // Calculate all. No.of half wavelengths (crossings), time-out
   emon2.calcVI(20, 2000);        // Calculate all. No.of half wavelengths (crossings), time-out
   emon3.calcVI(20, 2000);        // Calculate all. No.of half wavelengths (crossings), time-out
@@ -69,11 +72,6 @@ void emonget() {
   P3_powerFactor     = emon3.powerFactor;      //extract Power Factor into Variable
   P3_supplyVoltage   = emon3.Vrms;             //extract Vrms into Variable
   P3_Irms            = emon3.Irms;             //extract Irms into Variable
-
-  DS18B20.requestTemperatures(); // Send the command to get temperatures
-  temp = DS18B20.getTempCByIndex(0);
-  DS18B20.setWaitForConversion(false);
-  DS18B20.requestTemperatures();
 }
 
 void emonsendserial() {
@@ -132,20 +130,22 @@ void emonsendserial() {
   Serial.println();
 }
 
-void emonsendrf() {
+void emonsendtoesp() {
+  digitalWrite(LED_PIN, HIGH);
   char dataarray[32];
   sprintf(dataarray, ">1,%d,%d,%d,%d,%ld", P1_supplyVoltage, (int)(P1_Irms * 10), (int)(P1_powerFactor * 100), P1_realPower, (long)(P1_energy * 100));
-  radio.write(&dataarray, sizeof(dataarray));
+  ESPserial.println(dataarray);
   delay(150);
   sprintf(dataarray, ">2,%d,%d,%d,%d,%ld", P2_supplyVoltage, (int)(P2_Irms * 10), (int)(P2_powerFactor * 100), P2_realPower, (long)(P2_energy * 100));
-  radio.write(&dataarray, sizeof(dataarray));
+  ESPserial.println(dataarray);
   delay(150);
   sprintf(dataarray, ">3,%d,%d,%d,%d,%ld", P3_supplyVoltage, (int)(P3_Irms * 10), (int)(P3_powerFactor * 100), P3_realPower, (long)(P3_energy * 100));
-  radio.write(&dataarray, sizeof(dataarray));
+  ESPserial.println(dataarray);
   delay(150);
-  sprintf(dataarray, ">4,%d,%d", temp, error);
-  radio.write(&dataarray, sizeof(dataarray));
+  sprintf(dataarray, ">4,%d,%d", (int)(temp * 10), error);
+  ESPserial.println(dataarray);
   delay(150);
+  digitalWrite(LED_PIN, LOW);
 }
 
 void calcenergy() {
@@ -156,7 +156,9 @@ void calcenergy() {
 
 void setup() {
   Serial.begin(115200);
+  ESPserial.begin(9600);
   pinMode(PHASE_SEQ_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
   emon1.voltage(5, 256.2, 1.7);  // Voltage: input pin, calibration, phase_shift
   emon1.current(1, 62.59);       // Current: input pin, calibration.
   emon2.voltage(2, 250.9, 1.7);  // Voltage: input pin, calibration, phase_shift
@@ -164,17 +166,9 @@ void setup() {
   emon3.voltage(0, 255.9, 1.7);  // Voltage: input pin, calibration, phase_shift
   emon3.current(4, 61.66);       // Current: input pin, calibration.
   DS18B20.begin();
-  DS18B20.setResolution(10);
+  DS18B20.setResolution(12);
   DS18B20.setWaitForConversion(false);
   DS18B20.requestTemperatures();
-  radio.begin();
-  //radio.setRetries(15, 15);  // Set max retries and delay time on retries 15 max
-  //radio.setDataRate(RF24_250KBPS);
-  radio.setChannel(108);
-  radio.setAutoAck(false);
-  radio.setPALevel(RF24_PA_MAX);
-  radio.openWritingPipe(address);
-  radio.stopListening();
   timer.every(CALC_ENERGY_INTERVAL * 1000, calcenergy);
   delay(5000);
 }
@@ -183,7 +177,7 @@ void loop() {
   timer.update();
   emonget();
   emonsendserial();
-  emonsendrf();
+  emonsendtoesp();
   if (!digitalRead(PHASE_SEQ_PIN)) {
     error = 1;
   }
